ac100415f06af1250840a809327b0c4286cecec1
Akmal Ihab Syauqi <117333214+lil-baga@users.noreply.github.com>
2024-03-27 22:23:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('yes').addEventListener('click', function(){
         button.remove();
     });
     var img = document.getElementById('image');
-    img.src = '/gambar/teknisi.png';
+    img.src = 'teknisi.png';
     document.getElementById('pertanyaan').innerText = 'Anda Berbohong!';
     document.getElementById('container-utama').style.backgroundColor = '#E12E2E';
     document.getElementById('text').innerText = 'Anda adalah seorang Teknisi'
@@ -16,7 +16,7 @@ document.getElementById('no').addEventListener('click', function(){
         button.remove();
     });
     var img = document.getElementById('image');
-    img.src = '/gambar/mahasiswa.png';
+    img.src = 'mahasiswa.png';
     document.getElementById('pertanyaan').innerText = 'Anda Benar!';
     document.getElementById('container-utama').style.backgroundColor = '#6FD240';
     document.getElementById('text').innerText = 'Anda adalah seorang Mahasiswa'
