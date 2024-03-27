1d37059baa022ae4f39e2d441f3fe611ed06ca69
Akmal Ihab Syauqi <117333214+lil-baga@users.noreply.github.com>
2024-03-27 22:19:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var buttons = document.querySelectorAll('button')
+
+document.getElementById('yes').addEventListener('click', function(){
+    buttons.forEach(function(button) {
+        button.remove();
+    });
+    var img = document.getElementById('image');
+    img.src = '/gambar/teknisi.png';
+    document.getElementById('pertanyaan').innerText = 'Anda Berbohong!';
+    document.getElementById('container-utama').style.backgroundColor = '#E12E2E';
+    document.getElementById('text').innerText = 'Anda adalah seorang Teknisi'
+});
+
+document.getElementById('no').addEventListener('click', function(){
+    buttons.forEach(function(button) {
+        button.remove();
+    });
+    var img = document.getElementById('image');
+    img.src = '/gambar/mahasiswa.png';
+    document.getElementById('pertanyaan').innerText = 'Anda Benar!';
+    document.getElementById('container-utama').style.backgroundColor = '#6FD240';
+    document.getElementById('text').innerText = 'Anda adalah seorang Mahasiswa'
+});
